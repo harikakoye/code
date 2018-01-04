bcd8612661d3d856fc5102ea220856d68f737a7d
harikakoye <harikakoyi@gmail.com>
2018-01-04 17:34:29 +0530

    need latest

--- need tests.py
+++ need tests.py
@@ -55,3 +55,76 @@ class MatchedUserTest(APITestCase):
         response = self.client.get(url, data, format='json')
         print json.loads(response.content)
         self.assertEqual(json.loads(response.content),[{u'need': 1, u'interested': True, u'engaged': False, u'id': 1, u'user': 1}])
+'''
+class AgentRemarkTest(APITestCase):
+    maxDiff = None
+    """ Test module for MatchedUser model """
+    def setUp(self):
+        location = Location.objects.create(formatted_address = 'Kukatpally, Hyderabad, Telangana, India', country = 'India', administrative_area_level_1 = 'Telangana',
+            administrative_area_level_2='Ranga Reddy',sublocality_level_1='Kukatpally',sublocality_level_2='kukatpally 5th phase',
+            locality='Hyderabad',raw='[{"long_name": "Kukatpally", "types": ["sublocality_level_1"]}, {"long_name": "Hyderabad", "types": ["locality"]}, {"long_name": "Ranga Reddy", "types": ["administrative_area_level_2"]}, {"long_name": "Telangana", "types": ["administrative_area_level_1"]}, {"long_name": "India", "types": ["country"]}]',
+            lat='17.494793',lng='78.399644')
+        category = PropertyTypeCategory.objects.create(title = 'Commercial',description = 'power backup facility')
+        propertytype = PropertyType.objects.create(title = 'Showroom',category = category)
+        customer = Customer.objects.create(user_id=1,dob = '1995-07-01',is_frequent_buyer = 'False')
+        need = Need.objects.create(title='mfghjkh',need_type='land',status='active',desired_time='30 days',desired_budget_min=797887,desired_budget_max=997887,customer_notes ='bgjrhgjhn',location=location,propertytype=propertytype,customer=customer,is_expired='True')
+        user = User.objects.create(first_name='hamsini',last_name='hanvika')
+        AgentRemark.objects.create(need=need,user=user,remark='bad',rating=3)
+    def test_agentremark(self):
+        url = baseurl+'/api/agentremark/?rating=3'
+        data = {}
+        response = self.client.post(url, data, format='json')
+        print json.loads(response.content)
+        #self.assertEqual(json.loads(response.content),[{u'need': 1, u'interested': True, u'engaged': False, u'id': 1, u'user': 1}])
+
+
+class RemarkTest(APITestCase):
+    maxDiff = None
+    """ Test module for MatchedUser model """
+    def setUp(self):
+        location = Location.objects.create(formatted_address = 'Kukatpally, Hyderabad, Telangana, India', country = 'India', administrative_area_level_1 = 'Telangana',
+            administrative_area_level_2='Ranga Reddy',sublocality_level_1='Kukatpally',sublocality_level_2='kukatpally 5th phase',
+            locality='Hyderabad',raw='[{"long_name": "Kukatpally", "types": ["sublocality_level_1"]}, {"long_name": "Hyderabad", "types": ["locality"]}, {"long_name": "Ranga Reddy", "types": ["administrative_area_level_2"]}, {"long_name": "Telangana", "types": ["administrative_area_level_1"]}, {"long_name": "India", "types": ["country"]}]',
+            lat='17.494793',lng='78.399644')
+        category = PropertyTypeCategory.objects.create(title = 'Commercial',description = 'power backup facility')
+        propertytype = PropertyType.objects.create(title = 'Showroom',category = category)
+        customer = Customer.objects.create(user_id=1,dob = '1995-07-01',is_frequent_buyer = 'False')
+        need = Need.objects.create(title='mfghjkh',need_type='land',status='active',desired_time='30 days',desired_budget_min=797887,desired_budget_max=997887,customer_notes ='bgjrhgjhn',location=location,propertytype=propertytype,customer=customer,is_expired='True')
+        user = User.objects.create(first_name='hamsini',last_name='hanvika')
+        match = MatchedUser.objects.create(need=need,user=user,interested='True',engaged='False')
+        Remark.objects.create(match=match,remark='bad')
+    def test_remark(self):
+        url = baseurl+'/api/remark/?interested=True'
+        data = {}
+        response = self.client.get(url, data, format='json')
+        print json.loads(response.content)
+        self.assertEqual(json.loads(response.content),{u'status': u'success', u'response': [{u'remark': u'bad', u'id': 1, u'match': 1, u'time': u'11:19:51.652852'}]})
+'''
+class SuggestionTest(APITestCase):
+    maxDiff = None
+    """ Test module for MatchedUser model """
+    def setUp(self):
+        location = Location.objects.create(formatted_address = 'Kukatpally, Hyderabad, Telangana, India', country = 'India', administrative_area_level_1 = 'Telangana',
+            administrative_area_level_2='Ranga Reddy',sublocality_level_1='Kukatpally',sublocality_level_2='kukatpally 5th phase',
+            locality='Hyderabad',raw='[{"long_name": "Kukatpally", "types": ["sublocality_level_1"]}, {"long_name": "Hyderabad", "types": ["locality"]}, {"long_name": "Ranga Reddy", "types": ["administrative_area_level_2"]}, {"long_name": "Telangana", "types": ["administrative_area_level_1"]}, {"long_name": "India", "types": ["country"]}]',
+            lat='17.494793',lng='78.399644')
+        category = PropertyTypeCategory.objects.create(title = 'Commercial',description = 'power backup facility')
+        propertytype = PropertyType.objects.create(title = 'Showroom',category = category)
+        customer = Customer.objects.create(user_id=1,dob = '1995-07-01',is_frequent_buyer = 'False')
+        need = Need.objects.create(title='mfghjkh',need_type='land',status='active',desired_time='30 days',desired_budget_min=797887,desired_budget_max=997887,customer_notes ='bgjrhgjhn',location=location,propertytype=propertytype,customer=customer,is_expired='True')
+        user = User.objects.create(first_name='hamsini',last_name='hanvika')
+        matched_user = MatchedUser.objects.create(need=need,user=user,interested='True',engaged='False')
+        transactiontype = TransactionType.objects.create(name='buy')
+        state = State.objects.create(name = 'Telangana',country_code='IN')
+        country = Country.objects.create(name = 'India',country_code='IN',is_active='True')
+        address = Address.objects.create(line1 = 'prakash nagar',line2 = 'ial colony',landmark = '3rd lane',city = 'Secunderabad',state = state,country = country,pincode = '500003',lat='20.0',lng='77.0')
+        suggestion = Listing.objects.create(title='goldpillar',user_id=1,location=location,address=address,propertytype=propertytype,transactiontype=transactiontype,property_ownership='fjjg',description='tgirehtgr',society_name='dfjks',built_year=01-02-1995,posted_by='admin',listing_status='active',occupancy_status='inactive',
+                    available_from_month=01-02-1995,available_from_year=01-02-1995,engage_agents=True,is_expired=False,expected_price=500000,show_price_as=300000,price_per_sqft=25000,maintanence_charges=10000,duration=30 days,expected_rent_price=15000)
+        Suggestion.objects.create(matched_user=matched_user,suggestion=suggestion)
+    def test_suggestion(self):
+        url = baseurl+'/api/add_suggestion/?interested=True'
+        data = {}
+        response = self.client.get(url, data, format='json')
+        print json.loads(response.content)
+        #self.assertEqual(json.loads(response.content),{u'status': u'success', u'response': [{u'remark': u'bad', u'id': 1, u'match': 1, u'time': u'11:19:51.652852'}]})
+
