3b9048850687f9a2a171790c4293a4112a9455e2
harikakoye <harikakoyi@gmail.com>
2018-01-11 19:21:53 +0530

    regist

--- build tests.py
+++ build tests.py
@@ -26,16 +26,21 @@ class RegistrationTest(APITestCase):
             administrative_area_level_2='Ranga Reddy',sublocality_level_1='Kukatpally',sublocality_level_2='kukatpally 5th phase',
             locality='Hyderabad',raw='[{"long_name": "Kukatpally", "types": ["sublocality_level_1"]}, {"long_name": "Hyderabad", "types": ["locality"]}, {"long_name": "Ranga Reddy", "types": ["administrative_area_level_2"]}, {"long_name": "Telangana", "types": ["administrative_area_level_1"]}, {"long_name": "India", "types": ["country"]}]',
             lat='17.494793',lng='78.399644')
+        state = State.objects.create(name = 'Andhra Pradesh')
+        country = Country.objects.create(name = 'India',country_code='IN',is_active='True')
+        address = Address.objects.create(line1 = 'mg road',line2 = '40-1-48, Sri Krishna Sai Bhavan',landmark = 'Opp. D.V.Manor Hotel, M.G.Road, Labbipet',city = 'Vijayawada',state = state,country = country,pincode = 520010,lat='16.515099',lng='80.632095')
         newuser = User.objects.create(first_name='harika',last_name="hamsini",username="test",password="12345")
-        newcustomer = Customer.objects.create(user=newuser,is_frequent_buyer = 0)
-        newcustomer.contact_number.add(contact_number)
-        newcustomer.contact_email.add(contact_email)
-        newcustomer.location.add(location)
-        newcustomer.save()
-        url = baseurl+'/api/customer/registration/?'
+        newbuilder = Builder.objects.create(user=newuser)
+        newbuilder.contact_number.add(contact_number)
+        newbuilder.contact_email.add(contact_email)
+        newbuilder.location.add(location)
+        newbuilder.address.add(address)
+        newbuilder.save()
+        url = baseurl+'/api/builder/registration/?'
         data = {}
         data['otp'] = 306673
         data['first_name'] = "Harika"
+        data['last_name'] = "Hamsini"
         data["contact_number"] = "9908201775"
         data["locations"] = [{"location":[{"long_name":"Prakash Nagar","short_name":"Prakash Nagar","types":["sublocality_level_3","sublocality","political"]},{"long_name":"Bhagat Singh II","short_name":"Bhagat Singh II","types":["sublocality_level_2","sublocality","political"]},{"long_name":"Jogeshwari West","short_name":"Jogeshwari West","types":["sublocality_level_1","sublocality","political"]},{"long_name":"Mumbai","short_name":"Mumbai","types":["locality","political"]},{"long_name":"Mumbai Suburban","short_name":"Mumbai Suburban","types":["administrative_area_level_2","political"]},{"long_name":"Maharashtra","short_name":"MH","types":["administrative_area_level_1","political"]},{"long_name":"India","short_name":"IN","types":["country","political"]},{"long_name":"400047","short_name":"400047","types":["postal_code"]}],
         "lat":19.1547349,
@@ -46,6 +51,8 @@ class RegistrationTest(APITestCase):
         "lng":72.8656144,
         "formatted_address":"Mumbai, Maharashtra 400099, India"
         }],
+        data["company_name"]=''
+        data["company_website"]=''
         response = self.client.post(url, data, format='json')
         print json.loads(response.content)
         #self.assertEqual(json.loads(response.content),)
